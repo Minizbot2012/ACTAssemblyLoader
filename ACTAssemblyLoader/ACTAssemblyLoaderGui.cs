06188716fbcf5fb69de5d8753f3b011fde926bf3
minizbot2012 <mini@zbots.org>
2019-08-13 10:05:50 -0400

    Change log to a seperate tab; give a bit more detailed instructions in a multiline textbox; update ACTAssemblyLoader.cs

--- ACTAssemblyLoader/ACTAssemblyLoaderGui.cs
+++ ACTAssemblyLoader/ACTAssemblyLoaderGui.cs
@@ -9,12 +9,15 @@ namespace ACTAssemblyLoader
 {
     public partial class ACTAssemblyLoader : UserControl
     {
-        private Label labelAssemblies;
         private OpenFileDialog addDLLFile;
-        private CheckedListBox chklstAssemblies;
-        private ListView lstLog;
+        private TabControl tabControl;
+        private TabPage Tabs;
         private Button btnRemoveAssembly;
         private Button btnAddAssembly;
+        private CheckedListBox chklstAssemblies;
+        private TabPage tabPage2;
+        private ListView lstLog;
+        private TextBox txtInstructions;
         #region Designer Created Code (Avoid editing)
         /// <summary> 
         /// Required designer variable.
@@ -42,89 +45,129 @@ namespace ACTAssemblyLoader
         /// </summary>
         private void InitializeComponent()
         {
-            this.labelAssemblies = new System.Windows.Forms.Label();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(ACTAssemblyLoader));
             this.addDLLFile = new System.Windows.Forms.OpenFileDialog();
-            this.chklstAssemblies = new System.Windows.Forms.CheckedListBox();
-            this.lstLog = new System.Windows.Forms.ListView();
+            this.tabControl = new System.Windows.Forms.TabControl();
+            this.Tabs = new System.Windows.Forms.TabPage();
+            this.txtInstructions = new System.Windows.Forms.TextBox();
             this.btnRemoveAssembly = new System.Windows.Forms.Button();
             this.btnAddAssembly = new System.Windows.Forms.Button();
+            this.chklstAssemblies = new System.Windows.Forms.CheckedListBox();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.lstLog = new System.Windows.Forms.ListView();
+            this.tabControl.SuspendLayout();
+            this.Tabs.SuspendLayout();
+            this.tabPage2.SuspendLayout();
             this.SuspendLayout();
-            // 
-            // labelAssemblies
-            // 
-            this.labelAssemblies.AutoSize = true;
-            this.labelAssemblies.Dock = System.Windows.Forms.DockStyle.Top;
-            this.labelAssemblies.Location = new System.Drawing.Point(0, 0);
-            this.labelAssemblies.Name = "labelAssemblies";
-            this.labelAssemblies.Size = new System.Drawing.Size(569, 13);
-            this.labelAssemblies.TabIndex = 1;
-            this.labelAssemblies.Text = "Place assemblies to load in text box below, one per line. Ensure this plugin load" +
-    "s first, and restart ACT to apply changes.";
-            this.labelAssemblies.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
             // 
             // addDLLFile
             // 
             this.addDLLFile.FileName = "openFileDialog1";
             this.addDLLFile.Filter = "DLL|*.dll";
             // 
-            // chklstAssemblies
+            // tabControl
             // 
-            this.chklstAssemblies.Dock = System.Windows.Forms.DockStyle.Left;
-            this.chklstAssemblies.FormattingEnabled = true;
-            this.chklstAssemblies.Location = new System.Drawing.Point(0, 13);
-            this.chklstAssemblies.Name = "chklstAssemblies";
-            this.chklstAssemblies.Size = new System.Drawing.Size(450, 287);
-            this.chklstAssemblies.TabIndex = 2;
+            this.tabControl.Controls.Add(this.Tabs);
+            this.tabControl.Controls.Add(this.tabPage2);
+            this.tabControl.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tabControl.Location = new System.Drawing.Point(0, 0);
+            this.tabControl.Name = "tabControl";
+            this.tabControl.SelectedIndex = 0;
+            this.tabControl.Size = new System.Drawing.Size(700, 300);
+            this.tabControl.TabIndex = 6;
             // 
-            // lstLog
+            // Tabs
             // 
-            this.lstLog.Dock = System.Windows.Forms.DockStyle.Bottom;
-            this.lstLog.Enabled = false;
-            this.lstLog.HeaderStyle = System.Windows.Forms.ColumnHeaderStyle.Nonclickable;
-            this.lstLog.HideSelection = false;
-            this.lstLog.Location = new System.Drawing.Point(450, 200);
-            this.lstLog.Name = "lstLog";
-            this.lstLog.Size = new System.Drawing.Size(250, 100);
-            this.lstLog.TabIndex = 3;
-            this.lstLog.UseCompatibleStateImageBehavior = false;
-            this.lstLog.View = System.Windows.Forms.View.List;
+            this.Tabs.Controls.Add(this.txtInstructions);
+            this.Tabs.Controls.Add(this.btnRemoveAssembly);
+            this.Tabs.Controls.Add(this.btnAddAssembly);
+            this.Tabs.Controls.Add(this.chklstAssemblies);
+            this.Tabs.Location = new System.Drawing.Point(4, 22);
+            this.Tabs.Name = "Tabs";
+            this.Tabs.Padding = new System.Windows.Forms.Padding(3);
+            this.Tabs.Size = new System.Drawing.Size(692, 274);
+            this.Tabs.TabIndex = 0;
+            this.Tabs.Text = "Settings";
+            this.Tabs.UseVisualStyleBackColor = true;
+            // 
+            // txtInstructions
+            // 
+            this.txtInstructions.Dock = System.Windows.Forms.DockStyle.Right;
+            this.txtInstructions.Location = new System.Drawing.Point(456, 63);
+            this.txtInstructions.Multiline = true;
+            this.txtInstructions.Name = "txtInstructions";
+            this.txtInstructions.ReadOnly = true;
+            this.txtInstructions.Size = new System.Drawing.Size(233, 208);
+            this.txtInstructions.TabIndex = 8;
+            this.txtInstructions.Text = resources.GetString("txtInstructions.Text");
             // 
             // btnRemoveAssembly
             // 
             this.btnRemoveAssembly.Dock = System.Windows.Forms.DockStyle.Top;
-            this.btnRemoveAssembly.Location = new System.Drawing.Point(450, 13);
+            this.btnRemoveAssembly.Location = new System.Drawing.Point(453, 33);
             this.btnRemoveAssembly.Name = "btnRemoveAssembly";
-            this.btnRemoveAssembly.Size = new System.Drawing.Size(250, 30);
-            this.btnRemoveAssembly.TabIndex = 4;
+            this.btnRemoveAssembly.Size = new System.Drawing.Size(236, 30);
+            this.btnRemoveAssembly.TabIndex = 7;
             this.btnRemoveAssembly.Text = "Remove Selected Assembly";
             this.btnRemoveAssembly.UseVisualStyleBackColor = true;
-            this.btnRemoveAssembly.Click += new System.EventHandler(this.btnRemoveAssembly_Click);
             // 
             // btnAddAssembly
             // 
             this.btnAddAssembly.Dock = System.Windows.Forms.DockStyle.Top;
-            this.btnAddAssembly.Location = new System.Drawing.Point(450, 43);
+            this.btnAddAssembly.Location = new System.Drawing.Point(453, 3);
             this.btnAddAssembly.Name = "btnAddAssembly";
-            this.btnAddAssembly.Size = new System.Drawing.Size(250, 30);
-            this.btnAddAssembly.TabIndex = 5;
+            this.btnAddAssembly.Size = new System.Drawing.Size(236, 30);
+            this.btnAddAssembly.TabIndex = 6;
             this.btnAddAssembly.Text = "Add Assembly";
             this.btnAddAssembly.UseVisualStyleBackColor = true;
             this.btnAddAssembly.Click += new System.EventHandler(this.btnAddAssembly_Click);
+            // 
+            // chklstAssemblies
+            // 
+            this.chklstAssemblies.Dock = System.Windows.Forms.DockStyle.Left;
+            this.chklstAssemblies.FormattingEnabled = true;
+            this.chklstAssemblies.Location = new System.Drawing.Point(3, 3);
+            this.chklstAssemblies.Name = "chklstAssemblies";
+            this.chklstAssemblies.Size = new System.Drawing.Size(450, 268);
+            this.chklstAssemblies.TabIndex = 3;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Controls.Add(this.lstLog);
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(692, 274);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "Logs";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // lstLog
+            // 
+            this.lstLog.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.lstLog.Enabled = false;
+            this.lstLog.HeaderStyle = System.Windows.Forms.ColumnHeaderStyle.Nonclickable;
+            this.lstLog.HideSelection = false;
+            this.lstLog.Location = new System.Drawing.Point(3, 3);
+            this.lstLog.Name = "lstLog";
+            this.lstLog.Size = new System.Drawing.Size(686, 268);
+            this.lstLog.TabIndex = 4;
+            this.lstLog.UseCompatibleStateImageBehavior = false;
+            this.lstLog.View = System.Windows.Forms.View.List;
             // 
             // ACTAssemblyLoader
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
-            this.Controls.Add(this.btnAddAssembly);
-            this.Controls.Add(this.btnRemoveAssembly);
-            this.Controls.Add(this.lstLog);
-            this.Controls.Add(this.chklstAssemblies);
-            this.Controls.Add(this.labelAssemblies);
+            this.Controls.Add(this.tabControl);
             this.Name = "ACTAssemblyLoader";
             this.Size = new System.Drawing.Size(700, 300);
+            this.tabControl.ResumeLayout(false);
+            this.Tabs.ResumeLayout(false);
+            this.Tabs.PerformLayout();
+            this.tabPage2.ResumeLayout(false);
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
